08eb7767a6b74d24fef3eab961b2df1384e458e2
Nicole Pasquale <NSquale212@gmail.com>
2020-09-23 23:58:48 -0500

    Add config file

---
+++ src/server/config/index.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+const envFound = dotenv.config();
+
+if (!envFound) {
+    throw new Error('Could not find .env file!');
+}
+
+export default {
+    mysql: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
+        database: process.env.DB_SCHEMA
+    },
+    app: {
+        port: parseInt(process.env.PORT, 10),
+		api_prefix: process.env.API_PREFIX
+    },
+    auth : {
+        secret: process.env.JWT_SECRET
+    },
+    logs :{
+        morgan: process.env.MORGAN
+    }
+}
